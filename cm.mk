e860c60557858cdcd8ed37ed48078ec1a77fe88a
kirito96 <kirito96@users.noreply.github.com>
2016-10-24 12:28:02 -0400

    Switch to yaris

--- cm.mk
+++ cm.mk
@@ -2,21 +2,21 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := rainbow
+PRODUCT_RELEASE_NAME := yaris
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/wiko/rainbow/device_rainbow.mk)
+$(call inherit-product, device/alcatel/yaris/device_rainbow.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := rainbow
-PRODUCT_NAME := cm_rainbow
-PRODUCT_BRAND := wiko
-PRODUCT_MODEL := rainbow
-PRODUCT_MANUFACTURER := wiko
+PRODUCT_DEVICE := yaris
+PRODUCT_NAME := cm_yaris
+PRODUCT_BRAND := alcatel
+PRODUCT_MODEL := yaris
+PRODUCT_MANUFACTURER := alcatel
